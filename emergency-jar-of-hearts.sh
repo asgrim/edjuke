32be4a1c6a89e673fb60c830b9e15db7a9938bec
James Titcumb <james@asgrim.com>
2013-10-24 12:55:17 +0100

    Hide link

--- emergency-jar-of-hearts.sh
+++ emergency-jar-of-hearts.sh
@@ -2,5 +2,9 @@
 
 JAR_ID=`sqlite3 /home/pi/.config/xmms2/medialib.db "SELECT (c.position + 1) FROM Media AS m LEFT JOIN CollectionIdlists AS c ON c.mid = m.id WHERE m.value = 'Jar of Hearts';"`
 echo "Jar of Hearts id: $JAR_ID"
-xmms2 move -n $JAR_ID
-xmms2 next
+
+if [ "$JAR_ID" -gt "0" ]
+then
+  xmms2 move -n $JAR_ID
+  xmms2 next
+fi
